a75f82fa45c79f99dfb7e9c7bb057f042214b60f
KHOLIDIYAHAMNA <kholidiyahamna743@gmail.com>
2023-05-31 07:36:23 +0700

    membuat register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/register',[AuthController::class,'register'])->name('register');
+Route::get('/register',[AuthController::class,'registerPost'])->name('register');
